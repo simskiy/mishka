8e032f0b4a0225f2912956d8da461b3f98893b65
Simskiy <simskiy@gmail.com>
2020-09-15 18:58:55 +1000

    страница catalog. Стили на блоки catalog, card (tablet, desktop)

--- src/blocks/logo/logo.js
+++ src/blocks/logo/logo.js
@@ -2,21 +2,27 @@ let logo = document.querySelector('.logo__img');
 let img = document.querySelector('#logo__img');
 
 function changeLogo() {
-  if (document.body.clientWidth > 0) {
+  // if (document.body.innerWidth > 0) {
+  if (window.innerWidth > 0) {
     img.attributes['xlink:href'].value = 'img/svg/index-sprite.svg#logo-mobile';
     logo.attributes.width.value = '86';
     logo.attributes.height.value = '35';
   }
-  if (document.body.clientWidth >= 768) {
+  // if (document.body.innertWidth >= 768) {
+  if (window.innerWidth >= 768) {
     img.attributes['xlink:href'].value = 'img/svg/index-sprite.svg#logo-tablet';
     logo.attributes.width.value = '112';
     logo.attributes.height.value = '46';
   }
-  if (document.body.clientWidth >= 1150) {
+  // if (document.body.innertWidth >= 1150) {
+  if (window.innerWidth >= 1150) {
     img.attributes['xlink:href'].value = 'img/svg/index-sprite.svg#logo-desktop';
     logo.attributes.width.value = '149';
     logo.attributes.height.value = '72';
   }
+
+  // console.log('clientWidth: ' + document.body.clientWidth);
+  // console.log('innerWidth: ' + window.innerWidth);
 }
 
 window.addEventListener('resize', changeLogo);
